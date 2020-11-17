eb1461d403bf35c666c8257625d082c09c7987ae
Sergii-Tytarenko <tytarenko.sergiy@gmail.com>
2020-11-17 22:23:17 +0200

    update

--- #src/js/files/functions.js
+++ #src/js/files/functions.js
@@ -14,21 +14,7 @@ testWebP(function (support) {
 });
 
 
-//Smoth scroll header links
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-};
-
-//Smoth scroll header links
+//Smooth scroll header links
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', function (e) {

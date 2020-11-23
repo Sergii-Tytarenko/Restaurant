b40e8674d2a40c4dfac7101a3afda984be1e9fbf
Sergii-Tytarenko <tytarenko.sergiy@gmail.com>
2020-11-23 17:16:38 +0200

    update sleder

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -2,8 +2,17 @@
 let mySwiper = new Swiper('.reviews__container', {
     // Optional parameters
     loop: true,
+    autoHeight: true,
+
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
-    }
+    },
+
+    autoplay: {
+        delay: 6000,
+
+    },
+
+    speed: 1000,
   })
